89d7ae38cd214aa0b10a2b7ba898ffd3b0d32dc9
iJenders <jenders2906@gmail.com>
2026-03-05 19:04:23 -0400

    Agregar funciones para encender leds

--- code.c
+++ code.c
@@ -1,3 +1,75 @@
+// Funciones para encender leds (Así evitamos confundirnos con los argumenos)
+
+void EncenderLedVerde1()
+{
+	digitalWrite(Led1, HIGH);
+}
+
+
+void EncenderLedVerde2()
+{
+	digitalWrite(Led5, HIGH);
+}
+
+
+void EncenderLedVerde3()
+{
+	digitalWrite(Led9, HIGH);
+}
+
+
+void EncenderLedAmarillo1()
+{
+	digitalWrite(Led2, HIGH);
+}
+
+
+void EncenderLedAmarillo2()
+{
+	digitalWrite(Led6, HIGH);
+}
+
+
+void EncenderLedAmarillo3()
+{
+	digitalWrite(Led10, HIGH); 
+}
+
+void EncenderLedRojo1()
+{
+	digitalWrite(Led3, HIGH);
+}
+
+void EncenderLedRojo2()
+{
+	digitalWrite(Led7, HIGH);
+}
+
+void EncenderLedRojo3()
+{
+	digitalWrite(Led11, HIGH); 
+}
+
+
+void EncenderLedAzul1()
+{
+	digitalWrite(Led4, HIGH);
+}
+
+
+void EncenderLedAzul2()
+{
+	digitalWrite(Led8, HIGH);
+}
+
+
+void EncenderLedAzul3()
+{
+	digitalWrite(Led12, HIGH); 
+}
+
+// Ciclo de vida del arduino
+
 void setup()
 {
   for (int i = 0; i <= 13; i++) {
