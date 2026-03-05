892b4c625d2dfacbc7f7a094a4489620082f3bed
Reimil <reimilazuaje5toc@gmail.com>
2026-03-05 19:03:50 -0400

    Declaracion de los LEDs dependiendo del pin

--- code.c
+++ code.c
@@ -1,3 +1,19 @@
+//Declaracion de los LEDs dependiendo del pin donde este conectado
+int Led1 = 7;
+int Led2 = 6;
+int Led3 = 13;
+int Led4 = 12;
+int Led5 = 11;
+int Led6 = 10;
+
+int Led7 = 9;
+int Led8 = 8; 
+int Led9 = 5;
+int Led10 =4;
+int Led11 =3;
+int Led12 =2;
+
+
 void setup()
 {
   for (int i = 0; i <= 13; i++) {
